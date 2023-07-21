c6feb3c9a88c6f49a6d8c61b721eb9ada7bc12f1
aratius <arata1129matsu@icloud.com>
2023-07-21 16:39:38 +0900

    無限

--- Statement/statement.js
+++ Statement/statement.js
@@ -1,14 +1,14 @@
 const ws = require("ws");
 const statements = require("./endlessStatements");
 const baseStatement = require("./baseStatement");
-const config = require("./config")
-const print = require("./print")
+const config = require("./config");
+const print = require("./print");
 
 const server = new ws.Server({ port: 9000 });
 let isPrinting = false;
 
-const printer1 = config.printer1
-const printer2 = config.printer2
+const printer1 = config.printer1;
+const printer2 = config.printer2;
 
 const main = async () => {
 
@@ -27,10 +27,24 @@ const main = async () => {
     });
   });
 
+  setInterval(printEndless, 10);
+
 };
 
+let index = 0;
+let statementsCopied = [...statements];
 const printEndless = () => {
-
+  const statement = statementsCopied[index];
+  const max = Math.min(21, statement.length);
+  const text = statement.slice(0, max);
+  statementsCopied[index] = statement.slice(max, statement.length);
+  print(text, printer2);
+  console.log(text);
+  if (statementsCopied[index].length == 0) {
+    index++;
+    index = index % statements.length;
+    statementsCopied = [...statements];
+  }
 };
 
 main();
